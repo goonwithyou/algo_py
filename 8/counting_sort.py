7c62a7d18b1400bd7545c734ad3f29dae7a75a34
NiuCha Zhang <pe-tv@163.com>
2018-01-12 08:52:40 +0800

    计数排序
    Signed-off-by: NiuCha Zhang <pe-tv@163.com>

--- 8/counting_sort.py
+++ 8/counting_sort.py
@@ -4,3 +4,22 @@
 # @FileName: counting_sort.py
 # @Software: PyCharm Community Edition
 
+def counting_sort(A):
+    k = max(A)
+    l = len(A)
+    B = [None]*l
+    C = [0]*(k+1)
+
+    for i in range(l):
+        C[A[i]] = C[A[i]] + 1
+    for i in range(k):
+        C[i+1] = C[i+1] + C[i]
+    for i in range(l-1,-1,-1):
+        B[C[A[i]]-1] = A[i]
+        C[A[i]] = C[A[i]] - 1
+    return B
+
+#test
+A = [2,5,3,0,2,3,0,3,8,9,10,30,21,16]
+B = counting_sort(A)
+print(B)
